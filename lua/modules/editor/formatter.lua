b91403bf8b7bb406345a7e4df1feea73d35a5abe
LS8 <lennartschoettker@hotmail.com>
2021-03-16 21:47:14 +0100

    fix auto format undo error

--- lua/modules/editor/formatter.lua
+++ lua/modules/editor/formatter.lua
@@ -1,7 +1,10 @@
 vim.api.nvim_exec(
 [[
 augroup fmt
-  autocmd BufWritePre * undojoin | Neoformat
+  autocmd!
+  au BufWritePre * try | undojoin | Neoformat | catch /^Vim\%((\a\+)\)\=:E790/ | finally | silent Neoformat | endtry
 augroup END
 ]],
 false)
+
+-- from https://github.com/sbdchd/neoformat/issues/134
