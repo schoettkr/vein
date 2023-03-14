bd11e00112e67db66b50ffe85e4d8b77b0ff6e63
Lennart Schoettker <lschoettker@spotify.com>
2023-03-14 10:51:39 +0100

    add file browser plugin to telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -43,4 +43,44 @@ return {
       pcall(require('telescope').load_extension, 'fzf')
     end
   },
+  {
+    "nvim-telescope/telescope-file-browser.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+    config = function()
+      vim.api.nvim_set_keymap(
+        "n",
+        "<space>fp",
+        ":Telescope file_browser<CR>",
+        { noremap = true }
+      )
+
+      vim.api.nvim_set_keymap(
+        "n",
+        "<space>ff",
+        ":Telescope file_browser path=%:p:h select_buffer=true<CR>",
+        { noremap = true }
+      )
+
+
+      require("telescope").setup {
+        extensions = {
+          file_browser = {
+            theme = "ivy",
+            -- disables netrw and use telescope-file-browser in its place
+            hijack_netrw = true,
+            mappings = {
+              ["i"] = {
+                -- your custom insert mode mappings
+              },
+              ["n"] = {
+                -- your custom normal mode mappings
+              },
+            },
+          },
+        },
+                                 }
+      pcall(require("telescope").load_extension "file_browser")
+
+    end
+  }
 }
