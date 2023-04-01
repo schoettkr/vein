f3a4e27356c32dc2337942f445f51dcbac684ceb
Lennart Schoettker <lschoettker@spotify.com>
2023-04-01 19:10:50 +0200

    add vim surround

--- lua/plugins/general.lua
+++ lua/plugins/general.lua
@@ -1,13 +1,25 @@
 return {
-  -- { "folke/which-key.nvim",  opts = {} },
+	-- { "folke/which-key.nvim",  opts = {} },
 
-  -- Detect tabstop and shiftwidth automatically
-  { "tpope/vim-sleuth" },
+	-- Detect tabstop and shiftwidth automatically
+	{ "tpope/vim-sleuth" },
 
-  -- Comment things
-  { 'numToStr/Comment.nvim', opts = {} },
-  
-  {'windwp/nvim-autopairs',
-    config = function() require("nvim-autopairs").setup {} end
-  }
+	-- Comment things
+	{ 'numToStr/Comment.nvim', opts = {} },
+
+	{
+		'windwp/nvim-autopairs',
+		config = function() require("nvim-autopairs").setup {} end
+	},
+
+	{
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end
+	}
 }
