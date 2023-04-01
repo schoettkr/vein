753a8b92ab480e7369259fe762643c2df7a790ac
Lennart Schoettker <lschoettker@spotify.com>
2023-04-01 19:10:11 +0200

    format

--- lua/core/keybindings.lua
+++ lua/core/keybindings.lua
@@ -1,33 +1,33 @@
 local function map(mode, lhs, rhs, opts)
-    local options = {noremap = true}
-    if opts then
-        options = vim.tbl_extend("force", options, opts)
-    end
-    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+	local options = { noremap = true }
+	if opts then
+		options = vim.tbl_extend("force", options, opts)
+	end
+	vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
 local function cmd(mode, lhs, cmd_string, opts)
-  map(mode, lhs, ("<Cmd>%s<CR>"):format(cmd_string), opts)
+	map(mode, lhs, ("<Cmd>%s<CR>"):format(cmd_string), opts)
 end
 
 -- General Keybindings (vanilla vim)
-cmd("n", "<leader>Q",  "q!")
-cmd("n", "<leader>at",  "!alacritty &|")
-cmd("n", "<leader><tab>",  "b#")
+cmd("n", "<leader>Q", "q!")
+cmd("n", "<leader>at", "!alacritty &|")
+cmd("n", "<leader><tab>", "b#")
 cmd("n", "<leader>fs", "up")
 cmd("n", "<C-s>", "up")
 cmd("n", "<Bslash>", "noh")
 cmd("n", "<leader>bd", "bdelete")
 cmd("n", "<leader>bD", "bd!")
-cmd("n", "<leader>d",  "bdelete")
-cmd("n", "<leader>D",   "bd!")
-cmd("n", "<leader>bn",   "bnext")
-cmd("n", "<leader>bp",   "bprevious")
+cmd("n", "<leader>d", "bdelete")
+cmd("n", "<leader>D", "bd!")
+cmd("n", "<leader>bn", "bnext")
+cmd("n", "<leader>bp", "bprevious")
 cmd("n", "<leader>ws", "split")
 cmd("n", "<leader>wv", "vsplit")
 cmd("n", "<leader>wd", "close")
 cmd("n", "<leader>wm", "only")
-cmd("n", "<leader>qq", "quitall", {desc = "Quit"})
+cmd("n", "<leader>qq", "quitall", { desc = "Quit" })
 map("n", "<leader>wl", "<C-w>l")
 map("n", "<leader>wh", "<C-w>h")
 map("n", "<leader>wj", "<C-w>j")
@@ -36,19 +36,19 @@ map("n", "<leader>wL", "<C-w>L")
 map("n", "<leader>wH", "<C-w>H")
 map("n", "<leader>wJ", "<C-w>J")
 map("n", "<leader>wK", "<C-w>K")
-map("n", "<leader>1",  "1<C-w>w")
-map("n", "<leader>2",  "2<C-w>w")
-map("n", "<leader>3",  "3<C-w>w")
-map("n", "<leader>4",  "4<C-w>w")
-map("n", "<leader>5",  "5<C-w>w")
-map("n", "<leader>6",  "6<C-w>w")
+map("n", "<leader>1", "1<C-w>w")
+map("n", "<leader>2", "2<C-w>w")
+map("n", "<leader>3", "3<C-w>w")
+map("n", "<leader>4", "4<C-w>w")
+map("n", "<leader>5", "5<C-w>w")
+map("n", "<leader>6", "6<C-w>w")
 vim.cmd("vnoremap J :m '>+1<CR>gv=gv")
 vim.cmd("vnoremap K :m '<-2<CR>gv=gv")
-map("i", "<C-A>",  "<Home>")
-map("i", "<C-B>",  "<Left>")
-map("i", "<C-E>",  "<End>")
-map("i", "<C-F>",  "<Right>")
-map("i", "<C-Y>",  "<Esc>pa")
+map("i", "<C-A>", "<Home>")
+map("i", "<C-B>", "<Left>")
+map("i", "<C-E>", "<End>")
+map("i", "<C-F>", "<Right>")
+map("i", "<C-Y>", "<Esc>pa")
 
 
 -- Diagnostics
@@ -65,45 +65,50 @@ vim.api.nvim_set_keymap(
 	":Telescope file_browser<CR>",
 	{ noremap = true }
 )
-vim.api.nvim_set_keymap(
-	"n",
-	"<space>ff",
-	":Telescope file_browser path=%:p:h select_buffer=true<CR>",
-	{ noremap = true }
-)
+-- vim.api.nvim_set_keymap(
+-- 	"n",
+-- 	"<space>ff",
+-- 	":Telescope file_browser path=%:p:h select_buffer=true<CR>",
+-- 	{ noremap = true }
+-- )
 
 function telescope_builtin_mappings()
-    local telescope = require('telescope.builtin')
-    vim.keymap.set('n', '<leader>pp', telescope.find_files, { desc = 'Find project files'})
-    vim.keymap.set('n', '<leader>pf', telescope.find_files, { desc = 'Find project files'})
-    vim.keymap.set('n', '<leader>fr', telescope.oldfiles, { desc = 'Find recently opened files' })
-    vim.keymap.set('n', '<leader>gb', telescope.git_branches, { desc = 'Browse git branches' })
-    vim.keymap.set('n', '<leader>p/', telescope.live_grep, { desc = 'Search string in project' })
-    vim.keymap.set('n', '<leader>pw', telescope.grep_string, { desc = 'Search string under cursor' })
-    vim.keymap.set('n', '<leader>bb', telescope.buffers, { desc = 'Browse buffers' })
-    vim.keymap.set('n', '<leader>/', telescope.current_buffer_fuzzy_find, { desc = 'Search in current buffer' })
-    vim.keymap.set('n', '<leader>\'', telescope.resume, { desc = 'Resume last picker' })
-    vim.keymap.set('n', '<C-c><C-o>', telescope.quickfix, { desc = 'Show quickfix list' })
+	local telescope = require('telescope.builtin')
+	vim.keymap.set('n', '<leader>pp', telescope.find_files, { desc = 'Find project files' })
+	vim.keymap.set('n', '<leader>pf', telescope.find_files, { desc = 'Find project files' })
+	vim.keymap.set('n', '<leader>fr', telescope.oldfiles, { desc = 'Find recently opened files' })
+	vim.keymap.set('n', '<leader>gb', telescope.git_branches, { desc = 'Browse git branches' })
+	vim.keymap.set('n', '<leader>p/', telescope.live_grep, { desc = 'Search string in project' })
+	vim.keymap.set('n', '<leader>pw', telescope.grep_string, { desc = 'Search string under cursor' })
+	vim.keymap.set('n', '<leader>bb', telescope.buffers, { desc = 'Browse buffers' })
+	vim.keymap.set('n', '<leader>/', telescope.current_buffer_fuzzy_find, { desc = 'Search in current buffer' })
+	vim.keymap.set('n', '<leader>\'', telescope.resume, { desc = 'Resume last picker' })
+	vim.keymap.set('n', '<C-c><C-o>', telescope.quickfix, { desc = 'Show quickfix list' })
 end
+
 -- vim.keymap.set('n', '<C-c> <C-o>', telescope.quickfixhistory, { desc = 'Show quickfix history' })
 
 function telescope_mappings()
-    local actions = require "telescope.actions"
-    local actions_layout = require "telescope.actions.layout"
-    local mappings = {
-        i = {
-            -- ["<C-j>"] = actions.move_selection_next,
-            -- ["<C-k>"] = actions.move_selection_previous,
-            -- ["<C-n>"] = actions.cycle_history_next,
-            -- ["<C-p>"] = actions.cycle_history_prev,
-            ["?"] = actions_layout.toggle_preview,
-            ["<C-h>"] = "which_key",
-            -- https://github.com/nvim-telescope/telescope.nvim/blob/ccda9f02f3a7e48a4a84fb3c3e853d176af8bdd3/lua/telescope/actions/init.lua#L941
-            ["<C-c><C-o>"] = function(prompt_bufnr) actions.smart_send_to_qflist(prompt_bufnr) actions.open_qflist(prompt_bufnr) end,
-        },
-    }
-    return mappings
+	local actions = require "telescope.actions"
+	local actions_layout = require "telescope.actions.layout"
+	local mappings = {
+		i = {
+			-- ["<C-j>"] = actions.move_selection_next,
+			-- ["<C-k>"] = actions.move_selection_previous,
+			-- ["<C-n>"] = actions.cycle_history_next,
+			-- ["<C-p>"] = actions.cycle_history_prev,
+			["?"] = actions_layout.toggle_preview,
+			["<C-h>"] = "which_key",
+			-- https://github.com/nvim-telescope/telescope.nvim/blob/ccda9f02f3a7e48a4a84fb3c3e853d176af8bdd3/lua/telescope/actions/init.lua#L941
+			["<C-c><C-o>"] = function(prompt_bufnr)
+				actions.smart_send_to_qflist(prompt_bufnr)
+				actions.open_qflist(prompt_bufnr)
+			end,
+		},
+	}
+	return mappings
 end
+
 -- End Telescope Keybindings
 
 
@@ -168,7 +173,6 @@ function treesitter_textobject_mappings()
 	}
 end
 
-
 function treesitter_incselection_mappings()
 	return {
 		enable = true,
@@ -180,6 +184,7 @@ function treesitter_incselection_mappings()
 		},
 	}
 end
+
 -- End Treesitter Keybindings
 
 -- Filetree Keybindings
@@ -195,18 +200,18 @@ vim.keymap.set('n', '<leader>fF', "<cmd>NnnPicker<CR>", { desc = 'NNN Picker' })
 -- End Filetree Keybindings
 
 -- Git Keybdinings
-vim.keymap.set('n', '<leader>gs', "<cmd>Neogit<CR>", { desc = 'Git status'})
+vim.keymap.set('n', '<leader>gs', "<cmd>Neogit<CR>", { desc = 'Git status' })
 -- End Git Keybdinings
 
 -- Dired Keybindings
-vim.keymap.set('n', '<leader>ad', "<cmd>Dired<CR>", { desc = 'Dired'})
+vim.keymap.set('n', '<leader>ad', "<cmd>Dired<CR>", { desc = 'Dired' })
 kmap = vim.api.nvim_buf_set_keymap
 opt = { silent = true, noremap = true }
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = "dired",
 	callback = function()
-	    kmap(0, "n", "q", "<cmd>bdelete<CR>", opt)
-	    kmap(0, "n", "+", "<Plug>(dired_create)", opt)
+		kmap(0, "n", "q", "<cmd>bdelete<CR>", opt)
+		kmap(0, "n", "+", "<Plug>(dired_create)", opt)
 	end,
 })
 -- End Dired Keybindings
@@ -216,5 +221,5 @@ return {
 	telescope_builtin_mappings = telescope_builtin_mappings,
 	treesitter_textobject_mappings = treesitter_textobject_mappings,
 	treesitter_incselection_mappings = treesitter_incselection_mappings,
-  filetree_mappings = filetree_mappings
+	filetree_mappings = filetree_mappings
 }
